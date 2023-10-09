02450bbec927d9777648cf8b73391105a9704391
Beriquacker <gpandelet@gmail.com>
2023-10-09 16:33:36 +0200

    Actualizacion hora y fecha

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,15 @@
 import java.util.Scanner;
+import java.time.LocalDate;
 
+//...
 public class HelloWorld {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("¿Cuál es tu nombre?");
         String name = scanner.nextLine();
+
         System.out.println("¡Hola " + name + "!");
+        System.out.println("Hoy es: " + LocalDate.now());
     }
 }
 
